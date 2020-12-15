fa91ba9ee949831e6400da68eb183c62f833f86c
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-12-15 22:25:25 +0530

    firstall

---
+++ MyServiceDemo2/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyServiceDemo2.Models;
+
+namespace MyServiceDemo2.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+       private List<Employee> employees; 
+       public EmployeeController()
+       {
+           employees = new List<Employee>();
+           employees.Add(new Employee(){EmpNo=1,EmpName="A",Salary=13000});
+           employees.Add(new Employee(){EmpNo=2,EmpName="B",Salary=23000});
+           employees.Add(new Employee(){EmpNo=3,EmpName="C",Salary=33000});
+            
+       }    
+
+       [HttpGet]
+       public IActionResult Get()
+       {
+            return Ok(employees);
+       }
+       
+    }
+}
---
+++ MyServiceDemo2/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace MyServiceDemo2.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+    }
+}
